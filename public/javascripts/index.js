2c610ac032828f1896e3710a4647ee476c131d9d
Hassyunity <hassy.tsihoarana@gmail.com>
2025-04-09 21:54:26 +0300

    add contact page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,22 @@
 // Récupère tous les liens du menu
 const menuLinks = document.querySelectorAll('.menu ul li a');
 
+// Fonction pour appliquer la classe 'active' au lien correspondant à l'URL actuelle
+function setActiveLink() {
+  menuLinks.forEach(link => {
+    // Si l'URL du lien correspond à l'URL actuelle de la page
+    if (link.href === window.location.href) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+}
+
+// Appliquer la classe 'active' au chargement de la page
+setActiveLink();
+
+// Ajoute un événement de clic pour gérer l'ajout de la classe 'active' lors d'un clic
 menuLinks.forEach(link => {
   link.addEventListener('click', function() {
     // Retire la classe 'active' de tous les liens
