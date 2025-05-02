729116e66aea6ad5e0de8224db7283d4fbf0bc0b
Hassyunity <hassy.tsihoarana@gmail.com>
2025-05-02 11:56:48 +0300

    add project card

---
+++ public/javascripts/works.js
@@ -0,0 +1,15 @@
+const carousel = document.getElementById("carousel");
+    const cardWidth = 340; // largeur d'une carte + marge
+    let scrollX = 0;
+  
+    function maxScroll() {
+      return carousel.scrollWidth - carousel.parentElement.clientWidth;
+    }
+  
+    function scrollRight() {
+      scrollX += cardWidth;
+      if (scrollX > maxScroll()) {
+        scrollX = 0;
+      }
+      carousel.style.transform = `translateX(-${scrollX}px)`;
+    }
