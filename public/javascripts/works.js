fcf745e63e46b32caa2c8099c37b704e8b0e6a93
Hassyunity <hassy.tsihoarana@gmail.com>
2025-05-09 16:01:22 +0300

    add toggle menu

--- public/javascripts/works.js
+++ public/javascripts/works.js
@@ -1,15 +1,21 @@
 const carousel = document.getElementById("carousel");
-    const cardWidth = 340; // largeur d'une carte + marge
-    let scrollX = 0;
-  
-    function maxScroll() {
-      return carousel.scrollWidth - carousel.parentElement.clientWidth;
-    }
-  
-    function scrollRight() {
-      scrollX += cardWidth;
-      if (scrollX > maxScroll()) {
-        scrollX = 0;
-      }
-      carousel.style.transform = `translateX(-${scrollX}px)`;
-    }
+const cardWidth = 340; // largeur d'une carte + marge
+let scrollX = 0;
+
+function maxScroll() {
+  return carousel.scrollWidth - carousel.parentElement.clientWidth;
+}
+
+function scrollRight() {
+  scrollX += cardWidth;
+  if (scrollX > maxScroll()) {
+    scrollX = 0; // Retour au début du carrousel
+  }
+  carousel.style.transform = `translateX(-${scrollX}px)`;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Initialiser le carrousel pour afficher le premier projet
+  scrollX = 0;
+  carousel.style.transform = `translateX(-${scrollX}px)`;
+});
